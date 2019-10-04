2e810e4ef12b90185b2f50c5525ead59b4d2004d
Scott Bouchard <scottbouchard09@gmail.com>
2019-10-03 21:28:54 -0400

    add jopt for parsing. remove java.net

--- src/main/java/request/get.java
+++ src/main/java/request/get.java
@@ -1,23 +1,9 @@
 package request;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 
 public class get {
 
-    private HttpURLConnection connection;
-    private URL url;
-
-    private void sendGetRequest() throws IOException, MalformedURLException, ProtocolException {
-        String requestUrl = "";
-
-        url = new URL(requestUrl);
-        connection = (HttpURLConnection)url.openConnection();
-
-        connection.setRequestMethod("GET");
-
+    private void sendGetRequest() throws IOException {
     }
 }
